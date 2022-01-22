8913fde854ae7c3d062bf326eeda77cc38f3e26e
Brendan <brendanjackhorsley@gmail.com>
2022-01-21 21:51:35 -0500

    adding the election analysis

--- pypoll.py
+++ pypoll.py
@@ -38,29 +38,40 @@ with open(file_to_load) as election_data:
 
       candidate_votes[candidate_name] += 1
 
+with open(file_to_save, "w") as txt_file:
+   election_results = (
+      f"\nElection Rsults\n"
+      f"----------------------\n"
+      f"Total Votes: {total_votes:,}\n"
+      f"----------------------\n")
+
+   print(election_results, end="")
+
+   txt_file.write(election_results)
+
 #Determine vote percentage for each candidate
-for candidate_name in candidate_votes:
+   for candidate_name in candidate_votes:
       votes = candidate_votes[candidate_name]
       vote_percentage = float(votes) /float(total_votes) * 100
 
-   #Determine winning vote count and candidate 
+      #Determine winning vote count and candidate 
       if (votes > winning_count) and (vote_percentage > winning_percentage):
 
          winning_count = votes
          winning_percentage = vote_percentage
          winning_candidate = candidate_name
 
-winning_candidate_summary = (
-   f"----------------------\n"
-   f"Winner: {winning_candidate}\n"
-   f"Winning Vote Count: {winning_count:,}\n"
-   f"Winning Percentage: {winning_percentage:.1f}%\n"
-   f"----------------------\n")
+   winning_candidate_summary = (
+      f"----------------------\n"
+      f"Winner: {winning_candidate}\n"
+      f"Winning Vote Count: {winning_count:,}\n"
+      f"Winning Percentage: {winning_percentage:.1f}%\n"
+      f"----------------------\n")
 
-print(winning_candidate_summary)
+   print(winning_candidate_summary)
 
-
-
+   txt_file.write(winning_candidate_summary)
+   
 
 #close the file
 
